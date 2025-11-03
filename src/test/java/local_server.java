3defb2c84d1775084785b2e9aaa01dea9617c3a0
jaishankar-2003 <jaishankar98421@gmail.com>
2025-11-03 18:34:23 +0530

    test

--- src/test/java/local_server.java
+++ src/test/java/local_server.java
@@ -1,2 +1,99 @@
-public class local_server {
-}
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class local_server
+{
+
+    public static void main(String[] args)
+        {
+
+        // Base URL of your local API
+        RestAssured.baseURI = "http://localhost:3000";
+
+//            given()
+//                    .when()
+//                    .get("/students/abc0")
+//                    .then()
+//                    .statusCode(200)
+//                    .log().body();
+
+        /*
+
+
+
+        // ------------------ POST A NEW STUDENT ------------------
+        System.out.println("---- CREATE NEW STUDENT ----");
+        String requestBody = """
+            {
+              "name": "jai shankar",
+              "age": 21,
+              "gender": "Male",
+              "email": "jai.doe@example.com",
+              "course": "Cse Engineering",
+              "year": "Final",
+              "marks": {
+                "maths": 80,
+                "physics": 75,
+                "chemistry": 78
+              },
+              "status": "active"
+            }
+            """;
+
+        Response postResponse = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/students")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("jai shankar"))
+                .extract().response();
+
+        String studentId = postResponse.jsonPath().getString("id");
+        System.out.println("Created student ID: " + studentId);
+
+
+         */
+
+
+
+//
+//        // ------------------ PATCH (UPDATE STUDENT) ------------------
+//        System.out.println("---- UPDATE STUDENT ----");
+//        String patchBody = """
+//            {
+//              "course": "Testing"
+//            }
+//            """;
+//
+//        given()
+//                .contentType(ContentType.JSON)
+//                .body(patchBody)
+//                .when()
+//                .patch("/students/abc0")
+//                .then()
+//                .statusCode(200)
+//                .body("course", equalTo("Testing"))
+//                .log().body();
+//
+//
+
+
+        // ------------------ DELETE STUDENT ------------------
+        System.out.println("---- DELETE STUDENT ----");
+        given()
+               .when()
+               .delete("/students/")
+               .then()
+               .statusCode(200); // JSON Server returns 200 for delete success
+
+      //System.out.println("Student with ID " + studentId + " deleted successfully.");
+    }
+
+ }
+
